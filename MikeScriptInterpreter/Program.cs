364b03099ddc2f73534e294b1f4e8d74fa3abe78
Michael Little <mike@icbmike.com>
2018-04-07 17:35:11 +1200

    Add project files.

---
+++ MikeScriptInterpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MikeScriptInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
